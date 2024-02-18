db003aa11346cad9ec5b76fc131c99e83a7da205
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-18 20:20:23 +0800

    Changed indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,48 +4,48 @@ import { Grid } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ThemeWrapper>
-        <Grid container spacing={2}>
-          {/* Shopping Cart */}
-          <Grid item xs={8}>
-            <Container className="bg-slate-300 p4">
-              <Container className="flex justify-between">
-                <Typography variant="h4" component="h1">
-                  Shopping Cart
-                </Typography>
-                <Typography variant="h6" component="h2">
-                  Price
-                </Typography>
-              </Container>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <ThemeWrapper>
+                <Grid container spacing={2}>
+                    {/* Shopping Cart */}
+                    <Grid item xs={8}>
+                        <Container className="bg-slate-300 p4">
+                            <Container className="flex justify-between">
+                                <Typography variant="h4" component="h1">
+                                    Shopping Cart
+                                </Typography>
+                                <Typography variant="h6" component="h2">
+                                    Price
+                                </Typography>
+                            </Container>
 
-              <Divider />
+                            <Divider />
 
-              {/* Insert Items Here */}
+                            {/* Insert Items Here */}
 
-              {/* Subtotal float right */}
-              <Divider />
-              <Container className="flex justify-between">
-                <Typography variant="h6" component="h2">
-                  
-                </Typography>
-                <Typography variant="h6" component="h2">
-                  Subtotal (0 Items): $0.00
-                </Typography>
-              </Container>
+                            {/* Subtotal float right */}
+                            <Divider />
+                            <Container className="flex justify-between">
+                                <Typography variant="h6" component="h2">
 
-            </Container>
-          </Grid>
-          
-          {/* Checkout Button */}
-          <Grid item xs={4}>
-            <Container className="bg-slate-300 p-4">
+                                </Typography>
+                                <Typography variant="h6" component="h2">
+                                    Subtotal (0 Items): $0.00
+                                </Typography>
+                            </Container>
 
-            </Container>
-          </Grid>
-        </Grid>
-      </ThemeWrapper>
-    </main>
-  );
+                        </Container>
+                    </Grid>
+
+                    {/* Checkout Button */}
+                    <Grid item xs={4}>
+                        <Container className="bg-slate-300 p-4">
+
+                        </Container>
+                    </Grid>
+                </Grid>
+            </ThemeWrapper>
+        </main>
+    );
 }
