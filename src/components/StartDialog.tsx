4a2e48cb584a96d61651087eb80cdef0fbeaf95b
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-02-28 17:56:48 +0800

    Fixed lint warning

--- src/components/StartDialog.tsx
+++ src/components/StartDialog.tsx
@@ -11,7 +11,7 @@ const StartDialog: React.FC<StartDialogProps> = ({ open, onStart }) => (
     <DialogTitle>Start Timer</DialogTitle>
     <DialogContent>
       <DialogContentText>
-        Click "Start" to start the timer.
+        Click {"Start"} to start the timer.
       </DialogContentText>
     </DialogContent>
     <DialogActions>
