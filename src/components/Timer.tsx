1f243e971199bbc39316e8a6ec15bb7c9dcf5ba2
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-29 04:15:35 +0800

    Disable eslint `no-unused-vars` in Timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,7 +5,9 @@ interface TimerProps {
   onCheckout: () => void;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Timer: React.FC<TimerProps> = ({ start, onCheckout }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [timer, setTimer] = React.useState<number | null>(null);
 
   React.useEffect(() => {
