64db5c123a13729b5320526822b124ee2a8fd36c
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2024-03-14 12:28:30 +0800

    Added to changes to page configs

--- src/app/[configNum]/page.tsx
+++ src/app/[configNum]/page.tsx
@@ -99,9 +99,11 @@ export default function Home({ params }: { params: { configNum: string } }) {
     setGoalItems(newGoalItems);
 
     // Prepare task (Randomize the quantity of three items from cart in-place)
-    const randomIndices = Array.from({ length: 3 }, () =>
-      Math.floor(Math.random() * 7),
-    );
+    // Make sure no indices are repeated
+    const randomIndices = new Set<number>();
+    while (randomIndices.size < 3) {
+      randomIndices.add(Math.floor(Math.random() * 7));
+    }
 
     randomIndices.forEach((index) => {
       let randomQuantity = 0;
@@ -109,7 +111,7 @@ export default function Home({ params }: { params: { configNum: string } }) {
       // Ensure that the randomized quantity is different from the original quantity
       do {
         randomQuantity = Math.floor(Math.random() * 10) + 1;
-      } while (randomQuantity === randomizedCartItems[index].quantity);
+      } while (randomQuantity == randomizedCartItems[index].quantity);
 
       randomizedCartItems[index].quantity = randomQuantity;
     });
